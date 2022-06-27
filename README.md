1d71e0a67da7e18865d183f1e3327620c8be612a
prplblck <55458485+goodideagiver@users.noreply.github.com>
2022-06-27 11:27:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-parcel-template
+
+## How to use repository templates?
+https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template
+
+## How to start this project?
+Open this repo in your text editor, open console in project directory and use
+
+```npm install```
+
+```npm start```
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './assets/css/index.css';
+import './assets/css/variables.css';
+
+const App = () => {
+	return (
+		<h1
+			style={{
+				width: '100%',
+				textAlign: 'center',
+				fontSize: '2rem',
+				fontWeight: 'bold',
+				color: '#fff',
+				fontFamily: '"Roboto", sans-serif',
+			}}
+		>
+			Hello world
+		</h1>
+	);
+};
+
+export default App;
