1fb9bb2528a73998407db6326048cafeca27f72b
Karol <kariurek@gmail.com>
2022-06-28 08:17:42 +0200

    Add Base roadmap data handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import './assets/css/index.css';
 import './assets/css/variables.css';
 import { Roadmap } from './components/Roadmap/Roadmap';
+import { roadmapDataPoint } from './components/Roadmap/Roadmap.types';
 import { MainLayout } from './pages/MainLayout/MainLayout';
+
+const fakeRoadmapData: roadmapDataPoint[] = [
+	{
+		title: 'Siema',
+		finished: false,
+		date: new Date(2020, 20, 10),
+	},
+	{
+		title: 'Siema',
+		finished: false,
+		date: new Date(2020, 20, 10),
+	},
+	{
+		title: 'Siema',
+		finished: false,
+		date: new Date(2020, 20, 10),
+	},
+	{
+		title: 'Siema',
+		finished: false,
+		date: new Date(2020, 20, 10),
+	},
+];
 
 const App = () => {
 	return (
 		<MainLayout>
-			<Roadmap />
+			<Roadmap roadmapArray={fakeRoadmapData} />
 		</MainLayout>
 	);
 };
--- src/components/Roadmap/Roadmap.tsx
+++ src/components/Roadmap/Roadmap.tsx
@@ -1,5 +1,26 @@
-import { roadmapArray } from './Roadmap.types';
+import { Children } from 'react';
+import { RoadmapMainPoint } from './components/RoadmapMainPoint';
+import { roadmapArray, roadmapDataPoint } from './Roadmap.types';
 
 export const Roadmap = ({ roadmapArray }: { roadmapArray: roadmapArray }) => {
-	return <div>Roadmap</div>;
+	let roadmapData;
+
+	if (roadmapArray && roadmapArray.length > 0) {
+		roadmapData = roadmapArray.map((data: roadmapDataPoint) => {
+			const { title, date, dateFinished, midpoints, finished } = data;
+			return (
+				<RoadmapMainPoint
+					finished={finished}
+					date={date}
+					dateFinished={dateFinished}
+					title={title}
+					midpoints={midpoints}
+				/>
+			);
+		});
+	} else {
+		roadmapData = <div>Brak danych</div>;
+	}
+
+	return <div>{Children.toArray(roadmapData)}</div>;
 };
