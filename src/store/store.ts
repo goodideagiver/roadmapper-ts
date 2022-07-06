05209ffc45ea33db59c440e4927ccdc969622a05
Karol <kariurek@gmail.com>
2022-07-06 11:45:08 +0200

    Add roadmap slice to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { roadmapReducer } from './roadmap-slice';
 import { userReducer } from './user-settings-slice';
 
 export const store = configureStore({
 	reducer: {
 		userSettings: userReducer,
+		roadmap: roadmapReducer,
 	},
 });
