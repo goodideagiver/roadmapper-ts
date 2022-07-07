9b0a6f01b79db671040852b29abbfceedb72bd71
Karol <kariurek@gmail.com>
2022-07-07 11:56:52 +0200

    Add Roadmap length summary component base

--- src/components/RoadmapSummary/RoadmapSummary.tsx
+++ src/components/RoadmapSummary/RoadmapSummary.tsx
@@ -5,9 +5,8 @@ export const RoadmapSummary = () => {
 	const { mainRoadmapPoints } = useRoadmap();
 
 	const days = mainRoadmapPoints.filter((point) => point.daysToComplete);
-	console.log(days);
 
 	const totalDays = days.reduce((acc, curr) => acc + curr.daysToComplete, 0);
 
-	return <div>{totalDays}</div>;
+	return <div>{daysToYrsMthWeekDayString(totalDays)}</div>;
 };
