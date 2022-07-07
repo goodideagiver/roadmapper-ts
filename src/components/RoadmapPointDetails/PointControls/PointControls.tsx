f073cbfe7b635d66371afab7600b075ca5bd0cfc
Karol <kariurek@gmail.com>
2022-07-07 14:03:55 +0200

    Add Base for point controls component

---
+++ src/components/RoadmapPointDetails/PointControls/PointControls.tsx
@@ -0,0 +1,11 @@
+import classes from './PointControls.module.css';
+
+//change and display order
+//add midpoint to roadmap point
+//delete midpoint from roadmap
+//add point from roadmap
+//mark as finished
+
+export const PointControls = () => {
+	return <div>PointControls</div>;
+};
