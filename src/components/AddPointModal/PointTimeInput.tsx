3f819c31d43e0b27f3382926b7f6c4a8836bfe4b
prplblck <55458485+goodideagiver@users.noreply.github.com>
2022-07-17 15:27:36 +0200

    111+ years (#6)

--- src/components/AddPointModal/PointTimeInput.tsx
+++ src/components/AddPointModal/PointTimeInput.tsx
@@ -2,6 +2,7 @@ import { Button } from '../../UI/Button/Button';
 
 import { RoadmapPointTimeAdjust } from './components/RoadmapPointTimeAdjust/RoadmapPointTimeAdjust';
 import { TimeInputDisplay } from './components/TimeInputDisplay/TimeInputDisplay';
+import { LongDurationMessage } from './LongDurationMessage';
 
 type PointTimeInputProps = {
 	onChangeDays: (daysAmount: number) => void;
@@ -49,6 +50,15 @@ export const PointTimeInput = ({
 
 	return (
 		<div>
+			{daysValue > 3285 && daysValue < 6285 && (
+				<LongDurationMessage message='React was released in 2013' />
+			)}
+			{daysValue > 26280 ? (
+				<LongDurationMessage message='Entered number of days is longer than average human life expectancy.' />
+			) : null}
+			{daysValue > 9855 && daysValue < 11000 ? (
+				<LongDurationMessage message='JavaScript was invented in 1995' />
+			) : null}
 			<TimeInputDisplay
 				daysValue={daysValue}
 				daysFallback='No time specified'
