32e565b4fad1477bb945909ce11070b4a9ff26df
Karol <kariurek@gmail.com>
2022-08-01 10:23:12 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,34 @@ import { MainControls } from './components/MainControls/MainControls';
 import { Roadmap } from './components/Roadmap/Roadmap';
 import { RoadmapSummary } from './components/RoadmapSummary/RoadmapSummary';
 import 'react-notifications-component/dist/theme.css';
+import { parseUrlToRoadmapObject } from './helpers/parseRoadmapToURLString.helper';
 
 import { MainLayout } from './pages/MainLayout/MainLayout';
 import { useRoadmap } from './store/useRoadmap';
 import { MainFooter } from './components/MainFooter/MainFooter';
+import { useState } from 'react';
 
 const App = () => {
-	const { mainRoadmapPoints } = useRoadmap();
+	const { mainRoadmapPoints, setRoadmap } = useRoadmap();
 
+	const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+	if (window.location.pathname.includes('/share/') && isFirstLoad) {
+		const roadmapFromUrl = parseUrlToRoadmapObject(
+			window.location.pathname.replace('/share/', '')
+		);
+
+		// window.location.pathname = '/';
+		setRoadmap(roadmapFromUrl);
+
+		console.log(roadmapFromUrl);
+
+		// setRoadmap(roadmapFromUrl);
+	}
+
+	if (isFirstLoad) {
+		setIsFirstLoad(false);
+	}
 	return (
 		<MainLayout>
 			<ReactNotifications />
