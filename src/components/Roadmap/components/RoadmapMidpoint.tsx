c7aece7fb0bfcf0ef7a8f56095ddac8cd6a39264
prplblck <kariurek@gmail.com>
2022-08-03 20:37:50 +0200

    Remove unused files

--- src/components/Roadmap/components/RoadmapMidpoint.tsx
+++
@@ -1,5 +0,0 @@
-export const RoadmapMidpoint = () => {
-  return (
-    <div>RoadmapMidpoint</div>
-  )
-}
--- src/components/Roadmap/components/RoadmapPointDetail.tsx
+++
@@ -1,5 +0,0 @@
-export const RoadmapPointDetail = () => {
-  return (
-    <div>RoadmapPointDetail</div>
-  )
-}
